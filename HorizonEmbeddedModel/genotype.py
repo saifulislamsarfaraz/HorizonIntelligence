57a276d8b08386b8e25c2eeb0962f7bf1dfbde54
Saiful Islam Sarfaraz (Md. Saiful) <saifulislamsarfaraz@gmail.com>
2024-11-03 15:40:54 +0600

    ok

---
+++ HorizonEmbeddedModel/genotype.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a537c537d31ff55439957c48dfab3a6e6e501833092756209df3c1aa0834a89f
+size 5885
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90c27f24be47587d322106b9bdc821c6caf3e1d90630a621b81de5276898934a
+size 15848
